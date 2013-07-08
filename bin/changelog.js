9bdd652f17b1ab64440bc875cf2f388444b65209
Mike Larsson <mikelarssonftw@gmail.com>
2013-07-08 19:33:07 -0400

    Decouple from openbadges by pulling repo data into config and removing openbadges checkout. `bin/changelog.js setup` will clone the repo described by config.json to get started. config.sample.json shows what the openbadges config needs to look like.

--- bin/changelog.js
+++ bin/changelog.js
@@ -1,15 +1,51 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+const util = require('util');
+const optimist = require('optimist');
+
+const git = require ('../lib/git');
+
 const PORT = process.env['PORT'] || 3000;
 
-var app = require('../').app.build({
-  repo: {
-    owner: 'mozilla',
-    name: 'openbadges',
-    dir: './openbadges'
-  }
-});
+function readConfig(file) {
+  var contents = fs.readFileSync(file); 
+  return JSON.parse(contents);
+}
 
-app.listen(PORT, function() {
-  console.log("Listening on port " + PORT + ".");
-});
+var config = {};
+config.repo = readConfig(__dirname + '/../config.json');
+
+const argv = optimist
+  .usage('Usage: changelog.js [setup]')
+  .argv;
+
+if (argv.h) {
+  optimist.showHelp();
+  process.exit(0);
+}
+
+if (argv._[0] === 'setup') {
+  console.log('Installing...');
+  var url = util.format('https://github.com/%s/%s.git', config.repo.owner, config.repo.name);
+  var dir = __dirname + '/../' + config.repo.dir;
+  console.log(util.format('Cloning %s into %s...'), url, dir);
+  git.clone(url, dir, function(error) {
+    var errCode = 0;
+    if (error) {
+      console.log("Clone failed:", error);
+      errCode = 1;
+    }
+    else {
+      console.log("Setup successful!");
+    }
+    process.exit(errCode);
+  });
+}
+else {
+  var app = require('../').app.build(config);
+
+  app.listen(PORT, function() {
+    console.log("Listening on port " + PORT + ".");
+  });
+}
